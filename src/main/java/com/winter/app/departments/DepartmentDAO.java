b0812eac47e9de21f26b35f1c022a4284a5c50e9
merandie <kimdaeki@gmail.com>
2025-02-04 10:20:39 +0900

    1

---
+++ src/main/java/com/winter/app/departments/DepartmentDAO.java
@@ -0,0 +1,17 @@
+package com.winter.app.departments;
+
+public class DepartmentDAO {
+	
+	//부서 리스트
+	public void getList() {
+		//DB에서 부서 리스트를 조회
+		System.out.println("부서 리스트 조회");
+	}
+	
+	//한 부서의 모든 정보
+	public void getDetail() {
+		//DB에서 하나의 부서 정보를 조회
+		System.out.println("하나의 정보 조회");
+	}
+
+}
